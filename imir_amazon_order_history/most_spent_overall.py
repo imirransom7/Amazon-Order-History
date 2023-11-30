a9b15818cf935e5e30a63e59caa39b36264f7f80
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-30 14:55:25 -0500

    started making separate files for the data analysis

---
+++ imir_amazon_order_history/most_spent_overall.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('orderHistory.csv')
+df = df.fillna(0)
+
+
+# Creating a function to put any DataFrame in to use tabulate
+def tab(frame, formatting):
+    return tabulate(frame, headers='keys', tablefmt=formatting)
+
+
+# Creating a function that will be later used to implement a name based on the address in the DataFrame
+def get_name(name):
+    if name == 'Stephanie Pullins 3007 N STILLMAN ST PHILADELPHIA PA 19132-1306 United States'\
+            or name == 'Stephanie Pullins 2626 N BANCROFT ST PHILADELPHIA PA 19132-3933 United States':
+        return "Stephanie"
+    elif name == 'Ahmad Benefield 3007 N STILLMAN ST PHILADELPHIA PA 19132-1306 United States':
+        return "Ahmad"
+    elif name == 'Teyonna Pullins 3007 N STILLMAN ST PHILADELPHIA PA 19132-1306 United States':
+        return "Teyonna"
+    elif name == 'Sadie Graham 203 ATLANTIC AVE WILMINGTON DE 19804-1432 United States':
+        return "Sadie"
+    elif name == 'Jeffrey Mullen 203 ATLANTIC AVE WILMINGTON DE 19804-1432 United States':
+        return "Jeffrey"
+    elif name == 'Imir Ransom 1141 Creekside Dr Wilmington DE 19804 United States' \
+            or name == 'Imir 1140 CREEKSIDE DR APT 1 WILMINGTON DE 19804-3931 United States' \
+            or name == 'Imir Ransom 2005 CERVANTES CT NEWARK DE 19702-4401 United States' \
+            or name == 'Imir 2005 CERVANTES CT NEWARK DE 19702-4401 United States' \
+            or name == 'Imir 33 WENARK DR APT 12 NEWARK DE 19713-1442 United States' \
+            or name == 'Imir Ransom 83 PIKE CREEK RD APT 1B NEWARK DE 19711-6819 United States' \
+            or name == 'Imir Ransom 2 Indepence Hall Newark Delaware 19711 United States':
+        return "Imir"
+
+
+# Applying this function to the age column using the apply() method and assigning the result to a new coulumn called name
+df['Name'] = df['Shipping Address'].apply(get_name)
+
+overall_sum = df.groupby(['Name'])['Total Owed'].sum().reset_index()
+print(overall_sum)
---
+++ imir_amazon_order_history/testing.py
@@ -0,0 +1,71 @@
+# from tabulate import tabulate
+# import pandas as pd
+# import matplotlib.pyplot as plt
+#
+# # Read the CSV file
+# orders = pd.read_csv("orderHistory.csv")
+#
+# # Set maximum columns to display
+# pd.set_option("display.max_columns", 27)
+#
+# # to see all rows
+# # pd.set_option("display.max_rows", None)
+#
+# # Fill missing values with 0
+# orders = orders.fillna(0)
+#
+#
+# # Remove non-numeric characters and convert to float
+# orders["Shipment Item Subtotal"] = pd.to_numeric(orders["Shipment Item Subtotal"].str.replace('[^\d.]', '', regex=True), errors='coerce')
+# item_subtotal = orders["Shipment Item Subtotal"]
+#
+# columns_to_print = ["Shipping Address", "Product Name", "Shipment Item Subtotal", "Order Date"]
+# #print(order_history[columns_to_print])
+#
+# # This variable has all items ordered from the beginning of 2023 till the time I reqauted the Csv files of my Amazon order history
+# items_for_2023 = orders[orders["Order Date"] > "2023-01-01"]
+#
+# # This variable is going to show me all orders made to Stephanie Pullins
+# moms_orders = orders[orders["Shipping Address"] == "Stephanie Pullins 3007 N STILLMAN ST PHILADELPHIA PA 19132-1306 United States"]
+# # print(moms_orders)
+#
+# # print(tabulate(order_history.head(5), headers = "keys", tablefmt = "simple"))
+# # Sum the cleaned "Shipment Item Subtotal" column
+# subtotal_sum = item_subtotal.sum()
+# # print(subtotal_sum)
+# addresses = orders["Shipping Address"].unique()
+# print(addresses)
+#
+# order_names = orders.groupby(["Product Name", "Shipping Address"])["Unit Price"].count().reset_index()
+# print(order_names)
+#
+# order_names_pivot = order_names.pivot(
+#     columns="Shipping Address",
+#     index="Product Name",
+#     values="Unit Price"
+# ).reset_index()
+#
+# print(order_names_pivot)
+#
+#
+#
+#
+# # x = [1, 2, 3, 4, 5]
+# #
+# # y = [2, 4, 6, 8, 10]
+# #
+# # plt.plot(x, y)
+# #
+# # plt.show()
+#
+# def tab(frame, format):
+#     return tabulate(frame, headers='keys', tablefmt= format)
+#
+# # print(tab(orders, 'psql'))
+#
+#
+#
+#
+#
+#
+#
